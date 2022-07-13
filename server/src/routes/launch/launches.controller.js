850bceabb8ee1493066e84f56941a6d354ad0c0d
Haile-08 <haile4cmd@gmail.com>
2022-07-13 13:50:09 +0300

    added an error method for when the data is empty

--- server/src/routes/launch/launches.controller.js
+++ server/src/routes/launch/launches.controller.js
@@ -9,7 +9,23 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "missing requires data",
+    });
+  }
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "invalid launch date",
+    });
+  }
   addNewLaunche(launch);
   return res.status(201).json(launch);
 }
