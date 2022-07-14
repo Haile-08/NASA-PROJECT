868411db8e01250932024124709afee5656b4e17
Haile-08 <haile4cmd@gmail.com>
2022-07-14 12:05:53 +0300

    finished the post  test

--- server/src/routes/launch/launches.test.js
+++ server/src/routes/launch/launches.test.js
@@ -12,17 +12,28 @@ describe("Test GET /launches", () => {
 });
 ///post test
 describe("Test POST /launches", () => {
+  const completeLaunchData = {
+    mission: " uss ent",
+    rocket: "ncc",
+    target: " kepler",
+    launchDate: "january 4, 2028",
+  };
+  const launchdatawithoutdate = {
+    mission: " uss ent",
+    rocket: "ncc",
+    target: " kepler",
+  };
   test("it should respond with 201 created", async () => {
     const response = await request(app)
       .post("/launches")
-      .send({
-        mission: " uss ent",
-        rocket: "ncc",
-        target: " kepler",
-        launchDate: "january 4, 2028",
-      })
+      .send(completeLaunchData)
       .expect("Content-Type", /json/)
       .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+    expect(response.body).toMatchObject(launchdatawithoutdate);
   });
 
   test("it should catch missing required properites", () => {});
