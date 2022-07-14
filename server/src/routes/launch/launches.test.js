cbb0c00f77840ea9b05ada7f1df18e7616e05266
Haile-08 <haile4cmd@gmail.com>
2022-07-14 11:56:45 +0300

    created a post test

--- server/src/routes/launch/launches.test.js
+++ server/src/routes/launch/launches.test.js
@@ -1,15 +1,29 @@
 const request = require("supertest");
 const app = require("../../app");
-
+//get testing
 describe("Test GET /launches", () => {
   test("it should respond with 200 success", async () => {
-    const response = await request(app).get("/launches");
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
     expect(response.statusCode).toBe(200);
   });
 });
-
+///post test
 describe("Test POST /launches", () => {
-  test("it should respond with 200 success", () => {});
+  test("it should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: " uss ent",
+        rocket: "ncc",
+        target: " kepler",
+        launchDate: "january 4, 2028",
+      })
+      .expect("Content-Type", /json/)
+      .expect(201);
+  });
 
   test("it should catch missing required properites", () => {});
   test("it should catch invalis dates", () => {});
