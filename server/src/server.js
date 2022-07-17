320c8f2f9124c31038712729054bdff8479f138b
Haile-08 <haile4cmd@gmail.com>
2022-07-17 12:04:15 +0300

    created a mongodb database added a mongoose modules to talk to database

--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,9 @@ const { loadPlanetsData } = require("./models/planets.models");
 //COMMONLY react runs on port 3000 so the server needs to be differenet
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:0oZd1rmTFLlEQeEk@nasa.og6hzhn.mongodb.net/nasa?retryWrites=true&w=majority";
+
 const server = http.createServer(app);
 
 //load the csv data before the server.listen
