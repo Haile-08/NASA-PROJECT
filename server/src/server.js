6c01d5409a7ac420026467386b9acc69d237f79f
Haile-08 <haile4cmd@gmail.com>
2022-07-17 14:30:52 +0300

    added mongodb conntection

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const mongoose = require("mongoose");
+
 // HTTP is usefull for websockets for realtime communiection
 // express is just a middelware the we use as in this example
 // http allows us to respones to http request
@@ -15,8 +17,22 @@ const MONGO_URL =
 
 const server = http.createServer(app);
 
+mongoose.connection.on("open", () => {
+  console.log("Mongodb connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
 //load the csv data before the server.listen
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
